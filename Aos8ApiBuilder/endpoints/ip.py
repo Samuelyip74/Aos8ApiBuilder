ce6c79781f9e88b8cce65d78cc2ad36845020c16
Samuel Yip Kah Yean <samuel.yip@live.com>
2025-06-30 15:13:55 +0800

    build 40

--- Aos8ApiBuilder/endpoints/ip.py
+++ Aos8ApiBuilder/endpoints/ip.py
@@ -118,9 +118,7 @@ class IPInterfaceEndpoint(BaseEndpoint):
         mask: Optional[str] = None,
         device: str = "Vlan",
         vlan_id: Optional[int] = None,
-        local_proxy_arp: Optional[bool] = False,
-        encapsulation: Optional[str] = "e2",
-        primary: Optional[bool] = False
+        encap: Optional[str] = "e2",
     ) -> ApiResult:
         """
         Create a new IP interface with specified parameters.
@@ -145,35 +143,27 @@ class IPInterfaceEndpoint(BaseEndpoint):
             url = "/?domain=mib&urn=alaIpInterfaceTable"
 
             encapsulation_map = {
-                '1': 'e2',
-                '2': 'snap'
+                'e2': '1',
+                'snap': '2'
             }
-            encapsulation = encapsulation_map.get(device, '1')
+            encapsulation = encapsulation_map.get(encap, '1')
 
             device_type_map = {
-                '1': 'Vlan',
-                '2': 'GRE',
-                '3': 'IPIP',            
+                'Vlan': '1',
+                'GRE': '4',
+                'IPIP': '5',           
             }        
-            device_type = device_type_map.get(device, '1')
-
-            primary_config_map = {
-                '0': False,
-                '1': True,
-            }        
-            primary_config = primary_config_map.get(primary, '0')                      
+            device_type = device_type_map.get(device, '1')                 
 
             form_data = {
                 "mibObject0": f"ifIndex:{ifindex}",
                 "mibObject1": f"alaIpInterfaceAddress:{address}",
                 "mibObject2": f"alaIpInterfaceMask:{mask}",            
                 "mibObject3": f"alaIpInterfaceDeviceType:{device_type}",  
-                "mibObject4": "alaIpInterfacePortIfindex:0",
-                "mibObject5": f"alaIpInterfaceEncap:{encapsulation}",            
-                "mibObject6": f"alaIpInterfaceIpForward:1",                        
-                "mibObject8": f"alaIpInterfacePrimCfg:{primary_config}",                        
+                "mibObject4": f"alaIpInterfaceEncap:{encapsulation}",            
+                "mibObject5": f"alaIpInterfaceIpForward:1",                        
             }
-                # Conditionally add VLAN ID if provided
+
             if vlan_id is not None:
                 form_data["mibObject9"] = f"alaIpInterfaceVlanID:{str(vlan_id)}"
 
@@ -185,40 +175,100 @@ class IPInterfaceEndpoint(BaseEndpoint):
                 return response
         return response
 
-    def edit(
+    def edit_IP_Interface(
         self,
-        if_name: str,
+        ifindex: str,
         address: Optional[str] = None,
-        vip_address: Optional[str] = None,
         mask: Optional[str] = None,
-        admin_state: Optional[str] = None,
-        vlan: Optional[int] = None,
-        service: Optional[int] = None,
+        device: Optional[str] = None,
+        vlan_id: Optional[int] = None,
         forward: Optional[bool] = None,
         local_proxy_arp: Optional[bool] = None,
         encapsulation: Optional[str] = None,
         primary: Optional[bool] = None
     ) -> ApiResult:
         """
-        Edit an existing IP interface with updated parameters.
+        Create a new IP interface with specified parameters.
 
         Args:
-            if_name (str): Name of the interface.
+            ifindex (str): ifindex of the interface.
             address (Optional[str]): IP address.
-            vip_address (Optional[str]): Virtual IP address.
             mask (Optional[str]): Subnet mask.
-            admin_state (Optional[str]): Interface admin state, either 'enable' or 'disable'.
             vlan (Optional[int]): VLAN ID.
             service (Optional[int]): Associated service ID.
-            forward (Optional[bool]): Enable or disable packet forwarding.
-            local_proxy_arp (Optional[bool]): Enable or disable local proxy ARP.
             encapsulation (Optional[str]): Encapsulation type ('e2' or 'snap').
             primary (Optional[bool]): Set as primary interface.
 
         Returns:
-            ApiResult: Result of the edit operation or error response.
+            ApiResult: Result of the creation operation or error response.            
         """
-        ...
+        if ifindex:
+            url = "/?domain=mib&urn=alaIpInterfaceTable"
+       
+            form_data = {
+                "mibObject0": f"ifIndex:{ifindex}",
+                "mibObject1": f"alaIpInterfacePortIfindex:0",
+            }
+
+            if address is not None:
+                form_data["mibObject2"] = f"alaIpInterfaceAddress:{address}"
+
+            if mask is not None:
+                form_data["mibObject3"] = f"alaIpInterfaceMask:{mask}"   
+
+            if device is not None:
+                device_type_map = {
+                    'Vlan': '1',
+                    'GRE': '4',
+                    'IPIP': '5',            
+                }        
+                device_type_config = device_type_map.get(device, '1')                   
+                form_data["mibObject4"] = f"alaIpInterfaceDeviceType:{device_type_config}"   
+
+            if encapsulation is not None:
+                encapsulation_map = {
+                    'e2': '1',
+                    'snap': '2'
+                }
+                encapsulation_config = encapsulation_map.get(encapsulation, '1')
+                form_data["mibObject5"] = f"alaIpInterfaceEncap:{encapsulation_config}"   
+
+
+            if primary is not None:
+                primary_config_map = {
+                    False : '0',
+                    True : '1',
+                }        
+                primary_config = primary_config_map.get(primary, '0')                     
+                form_data["mibObject6"] = f"alaIpInterfacePrimCfg:{str(primary_config)}"                           
+                            
+            if vlan_id is not None:
+                form_data["mibObject7"] = f"alaIpInterfaceVlanID:{str(vlan_id)}"
+
+            if forward is not None:
+                forward_config_map = {
+                    False: '2',
+                    True: '1'
+                }        
+                forward_config = forward_config_map.get(forward, '1')                     
+                form_data["mibObject8"] = f"alaIpInterfaceIpForward:{forward_config}"   
+
+            if local_proxy_arp is not None:
+                local_proxy_arp_map = {
+                    False: '2',
+                    True: '1'
+                }        
+                local_proxy_arp_config = local_proxy_arp_map.get(local_proxy_arp, '1')                     
+                form_data["mibObject9"] = f"alaIpInterfaceLocalProxyArp:{local_proxy_arp_config}"                                  
+
+            response = self._client.post(url, data=form_data)
+            if response.success:
+                response = self.list()
+                return response
+            return response
+        return None
+
+   
 
     def delete(self, name: str) -> ApiResult:
         """
--- Aos8ApiBuilder/main.py
+++ Aos8ApiBuilder/main.py
@@ -10,14 +10,16 @@ client = (
 )
 
 result = client.ip.create_IP_Interface(
-    name="int-999",
-    address = "1.1.1.99",
-    mask = "255.255.255.0",
-    vlan_id = 99,
-)
+    name="vlan-999",
+    address="11.1.1.99",
+    mask="255.255.255.0",
+    device="Vlan",
+    vlan_id=99,
+    local_proxy_arp=True
+    )
 if result.success:
     print("✅ Operation successfully")
-    pprint.pprint(result.data)
+    #pprint.pprint(result.data)
 else:
     print(f"❌ Operation failed (diag={result.diag}): {result.error}")
 
